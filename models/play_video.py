a808ab83bbaf52e24f793c9dc9bd4d1364e6979a
4RnoLD31 <pavelmilosh31@gmail.com>
2023-06-13 17:05:55 +0300

    An attempt to improve optimization. Minor edits

--- models/play_video.py
+++ models/play_video.py
@@ -1,27 +1,33 @@
-import tkinter as tk
+from tkinter import *
 from PIL import Image, ImageTk
 import cv2
 import utils.constants
 
 
 class MainWindow:
-    def __init__(self, window, canvas, img_bg):
+    def __init__(self, window, canvas, picture, speed=20):
         self.window = window
         self.canvas = canvas
-        self.img_bg = img_bg
-        self.cap = cv2.VideoCapture(self.img_bg)
-        self.speed = 20  # Interval in ms to get the latest frame
-        self.y = 20
+        self.picture = picture
+        self.cap = cv2.VideoCapture(self.picture)
+        self.speed = speed
         self.update_image()
 
     def update_image(self):
         try:
-            self.image = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)  # to RGB
-            self.image = Image.fromarray(self.image)  # to PIL format
-            self.image = ImageTk.PhotoImage(self.image)  # to ImageTk format
-            self.frame = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image)
+            self.clear()
+            self.imageTK = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)))  # to ImageTk format
+            self.frame = self.canvas.create_image(0, 0, anchor=NW, image=self.imageTK)
             self.canvas.tag_lower(self.frame)
             self.window.after(self.speed, self.update_image)
         except:
-            MainWindow(utils.constants.statistics_club_window, utils.constants.statistics_club_canvas, self.img_bg)
+            self.clear()
+            MainWindow(utils.constants.statistics_club_window, utils.constants.statistics_club_canvas, self.picture)
+            del self
 
+    def clear(self):
+        try:
+            del self.imageTK
+            self.canvas.delete(self.frame)
+        except:
+            pass
